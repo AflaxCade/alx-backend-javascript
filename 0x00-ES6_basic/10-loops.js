00a4e2205aa06f68f0fb0c57074098f944b195dc
AFLAH <aflah.cade@gmail.com>
2024-01-02 18:00:14 +0300

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const newarray = [];
   for (let value of array) {
-    value = appendString + value;
+    newarray.push(appendString + value);
   }
 
-  return array;
+  return newarray;
 }
