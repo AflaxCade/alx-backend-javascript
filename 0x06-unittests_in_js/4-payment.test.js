fc0e82a005cb42bd96c229bdcc4cb105798c1105
AflaxCade <aflah.cade@gmail.com>
2024-02-29 22:04:52 +0000

    4-payment.test.js

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,24 +1,18 @@
-const { expect } = require('chai');
 const sinon = require('sinon');
-
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  const consoleSpy = sinon.spy(console, 'log');
-  it('validates usage of Utils.calculateNumber', () => {
-    // Stub the function always return the same number 10
-    const calcNumStub = sinon.stub(Utils, 'calculateNumber').returns(10);
-    // run the function with the parameters
-    sendPaymentRequestToApi(100, 20);
-    // check that the stub is being called with type = SUM, a = 100, and b = 20
-    expect(calcNumStub.calledWith('SUM', 100, 20)).to.be.true;
-    // check that the stub always return the same number 10
-    expect(calcNumStub.alwaysReturned(10)).to.be.true;
-    // check with spy that console.log is logging the correct message 
-    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+    it('should spy on the add function', () => {
+        const stubtub = sinon.stub(Utils, 'calculateNumber')
+        stubtub.returns(10)
+        const JB = sinon.spy(console, 'log')
 
-    calcNumStub.restore();
-    consoleSpy.restore();
-  });
-});
+        sendPaymentRequestToApi(100, 20)
+        sinon.assert.calledWith(stubtub, 'SUM', 100, 20)
+        sinon.assert.calledWith(JB, 'The total is: 10');
+        stubtub.restore()
+        JB.restore()
+    })
+})
